e4305a853dc2a87830dbd4ed5c68ac3b36b0c2d7
aiden <aidenrphotos@gmail.com>
2023-04-02 10:34:07 -0400

    Add ball and wall collision

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,17 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1000, 600), "Pong");
+	window.setFramerateLimit(60);
+
+	//  Draw ball
+	float stepx = 5;
+	float stepy = 5;
+
+	sf::RectangleShape ball;
+	ball.setSize(sf::Vector2f(10, 10));
+	ball.setFillColor(sf::Color(240, 240, 240));
+	ball.setOrigin(5, 5);
+	ball.setPosition(495, 295);
 
 	while (window.isOpen())
 	{
@@ -15,7 +26,28 @@ int main()
 			}
 		}
 
+		// If the ball hits the left or right side reverse the direction
+		if (ball.getPosition().x > 995)
+		{
+			stepx = -5;
+		}
+		else if (ball.getPosition().x < 5)
+		{
+			stepx = 5;
+		}
+
+		if (ball.getPosition().y > 595)
+		{
+			stepy = -5;
+		}
+		else if (ball.getPosition().y < 5)
+		{
+			stepy = 5;
+		}
+
+		ball.move(stepx, stepy);
 		window.clear();
+		window.draw(ball);
 		window.display();
 	}
 	return 0;
