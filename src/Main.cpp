119be076fddfc80c3b39236868a8c830695620ff
aiden <aidenrphotos@gmail.com>
2023-04-05 11:13:31 -0400

    merge paddle classes into one class

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,7 @@
 
 #include <ball.hpp>
 #include <net.hpp>
-#include <paddle1.hpp>
-#include <paddle2.hpp>
+#include <paddles.hpp>
 
 int main()
 {
@@ -14,8 +13,8 @@ int main()
 	Ball ball(10, 10, 595, 295);
 
 	// Create player paddles
-	Paddle1 paddle1(10, 40, 20, 280);
-	Paddle2 paddle2(10, 40, 980, 280);
+	Paddles leftPaddle(10, 40, 20, 280);
+	Paddles rightPaddle(10, 40, 980, 280);
 
 	// Draw dotted line
 	Net net(3, 15);
@@ -33,32 +32,31 @@ int main()
 			// Check if s or w has been pressed
 			if (event.type == sf::Event::KeyPressed)
 			{
-				paddle1.processEvents(event.key.code, true);
+				leftPaddle.processLeftEvents(event.key.code, true);
 			}
 			else if (event.type == sf::Event::KeyReleased)
 			{
-				paddle1.processEvents(event.key.code, false);
+				leftPaddle.processLeftEvents(event.key.code, false);
 			}
 
 			// Check if up or down arrow has been pressed
 			if (event.type == sf::Event::KeyPressed)
 			{
-				paddle2.processEvents(event.key.code, true);
+				rightPaddle.processRightEvents(event.key.code, true);
 			}
 			else if (event.type == sf::Event::KeyReleased)
 			{
-				paddle2.processEvents(event.key.code, false);
+				rightPaddle.processRightEvents(event.key.code, false);
 			}
 		}
 
 		window.clear(sf::Color(6, 6, 6));
 		ball.update();
 		ball.draw_to(window);
-		paddle1.update();
-		paddle1.drawTo(window);
-		paddle2.update();
-		paddle2.drawTo(window);
-		net.drawTo(window);
+		leftPaddle.updateLeft();
+		leftPaddle.leftDrawTo(window);
+		rightPaddle.updateRight();
+		rightPaddle.rightDrawTo(window);
 		net.drawTo(window);
 		window.display();
 	}
---
+++ src/paddles.hpp
@@ -0,0 +1,142 @@
+#ifndef PADDLES_HPP
+#define PADDLES_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Paddles
+{
+public:
+	Paddles(float height, float width, float x, float y)
+	{
+		// Define left paddle components
+		leftPaddle.setPosition(x, y);
+		leftPaddle.setSize(sf::Vector2f(height, width));
+		leftPaddle.setOrigin(height / 2, width / 2);
+		leftPaddle.setFillColor(sf::Color(200, 200, 200));
+		leftUp = false;
+		leftDown = false;
+
+		// Define right paddle components
+		rightPaddle.setPosition(x, y);
+		rightPaddle.setSize(sf::Vector2f(height, width));
+		rightPaddle.setOrigin(height / 2, width / 2);
+		rightPaddle.setFillColor(sf::Color(200, 200, 200));
+		rightUp = false;
+		rightDown = false;
+	}
+
+	void processLeftEvents(sf::Keyboard::Key key, bool checkPressed)
+	{
+		// Check if the left paddle is being controlled
+		if (checkPressed == true)
+		{
+			if (key == sf::Keyboard::W)
+			{
+				leftDown = true;
+			}
+			else if (key == sf::Keyboard::S)
+			{
+				leftUp = true;
+			}
+		}
+
+		if (checkPressed == false)
+		{
+			leftUp = false;
+			leftDown = false;
+		}
+	}
+
+	void processRightEvents(sf::Keyboard::Key key, bool checkPressed)
+	{
+
+		// Check if the right paddle is being controlled
+		if (key == sf::Keyboard::Up)
+		{
+			rightDown = true;
+		}
+		else if (key == sf::Keyboard::Down)
+		{
+			rightUp = true;
+		}
+
+		if (checkPressed == false)
+		{
+			rightUp = false;
+			rightDown = false;
+		}
+	}
+
+	void updateLeft()
+	{
+		sf::Vector2f leftMovement;
+
+		// Control the direction of the left paddle
+		// and stop it at the top and bottom of window
+		if (leftUp)
+		{
+			leftMovement.y += 15;
+			if (leftPaddle.getPosition().y >= 580)
+			{
+				leftMovement.y -= 15;
+			}
+		}
+		else if (leftDown)
+		{
+			leftMovement.y -= 15;
+			if (leftPaddle.getPosition().y <= 20)
+			{
+				leftMovement.y += 15;
+			}
+		}
+
+		leftPaddle.move(leftMovement);
+	}
+
+	void updateRight()
+	{
+		sf::Vector2f rightMovement;
+
+		// Control the direction of the right paddle
+		// and stop it at the top and bottom of window
+		if (rightUp)
+		{
+			rightMovement.y += 15;
+			if (rightPaddle.getPosition().y >= 580)
+			{
+				rightMovement.y -= 15;
+			}
+		}
+		else if (rightDown)
+		{
+			rightMovement.y -= 15;
+			if (rightPaddle.getPosition().y <= 20)
+			{
+				rightMovement.y += 15;
+			}
+		}
+
+		rightPaddle.move(rightMovement);
+	}
+
+	// Draw left paddle to window
+	void leftDrawTo(sf::RenderWindow& window)
+	{
+		window.draw(leftPaddle);
+	}
+
+	// Draw right paddle to window
+	void rightDrawTo(sf::RenderWindow& window)
+	{
+		window.draw(rightPaddle);
+	}
+
+private:
+	sf::RectangleShape leftPaddle;
+	bool leftUp, leftDown;
+
+	sf::RectangleShape rightPaddle;
+	bool rightUp, rightDown;
+};
+
+#endif
